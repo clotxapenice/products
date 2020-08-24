2cd2bf686dafda70af8dc5cfda6dd88e8f69294c
Kryston <clotxapenice@yahoo.com.tw>
2020-08-24 23:28:39 +0800

    add read products

--- products.py
+++ products.py
@@ -1,14 +1,27 @@
+#讀寫檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8')as d:
+    for line in d:
+        if '商品,價格' in line:
+            continue
+        name, price = line.strip('\n').split(',')
+        products.append([name, price])
+#購買紀錄
 while True:
     name = input('請輸入商品名稱:')
     if name == 'q':
        break
     price = input('請輸入價格')
     products.append([name ,price])
-for p in products:
-    print(p[0] ,'的價格為' ,p[1])
+#寫入檔案
+with open('products.csv', 'w', encoding='utf-8')as f: 
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+            
+            
 
-with open('products.csv', 'w', encoding='utf-8')as f:
-    f.write('商品,價格\n')
-    for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+                
+
+
+
