0a0245e1cc6eca40ec612594605d456e5113fa78
Kryston <clotxapenice@yahoo.com.tw>
2020-08-27 16:21:14 +0800

    new commit

--- products.py
+++ products.py
@@ -1,24 +1,35 @@
+import os
 #讀寫檔案
-products = []
-with open('products.csv', 'r', encoding='utf-8')as d:
-    for line in d:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip('\n').split(',')
-        products.append([name, price])
-#購買紀錄
-while True:
-    name = input('請輸入商品名稱:')
-    if name == 'q':
-       break
-    price = input('請輸入價格')
-    products.append([name ,price])
+def read_file(filename):
+    if os.path.isfile(filename):
+        products = []
+        with open(filename, 'r', encoding='utf-8')as d:
+            for line in d:
+                if '商品,價格' in line:
+                    continue
+                name, price = line.strip('\n').split(',')
+                products.append([name, price])
+        return products
+#輸入購買紀錄
+def input_(products):
+    while True:
+        name = input('請輸入商品名稱:')
+        if name == 'q':
+            break
+        price = input('請輸入價格')
+        price = int(price)
+        products.append([name ,price])
+    return products
 #寫入檔案
-with open('products.csv', 'w', encoding='utf-8')as f: 
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8')as f: 
         f.write('商品,價格\n')
         for p in products:
-            f.write(p[0] + ',' + p[1] + '\n')
-            
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+products = read_file('products.csv')
+products = input_(products)
+write_file('products.csv', products)                           
             
 
                 
